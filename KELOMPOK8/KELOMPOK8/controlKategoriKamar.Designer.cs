ae2f34bb1744f7b4709cb459c7fea9c20c828e07
yulitaa <ynazizah26@gmail.com>
2014-06-28 14:22:34 +0700

    Desain Insert Update Delete ControlKategoriKamar

--- KELOMPOK8/KELOMPOK8/controlKategoriKamar.Designer.cs
+++ KELOMPOK8/KELOMPOK8/controlKategoriKamar.Designer.cs
@@ -33,6 +33,12 @@
             this.buttonHapus = new System.Windows.Forms.Button();
             this.buttonUpdate = new System.Windows.Forms.Button();
             this.buttonTambah = new System.Windows.Forms.Button();
+            this.labelNamaKategori = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxNamaKategori = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGVKategoriKamar)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,18 +57,18 @@
             // 
             this.dataGVKategoriKamar.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.dataGVKategoriKamar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGVKategoriKamar.Location = new System.Drawing.Point(16, 45);
+            this.dataGVKategoriKamar.Location = new System.Drawing.Point(16, 103);
             this.dataGVKategoriKamar.Name = "dataGVKategoriKamar";
-            this.dataGVKategoriKamar.Size = new System.Drawing.Size(650, 450);
+            this.dataGVKategoriKamar.Size = new System.Drawing.Size(761, 383);
             this.dataGVKategoriKamar.TabIndex = 10;
             // 
             // buttonHapus
             // 
             this.buttonHapus.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.buttonHapus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonHapus.Location = new System.Drawing.Point(687, 152);
+            this.buttonHapus.Location = new System.Drawing.Point(696, 45);
             this.buttonHapus.Name = "buttonHapus";
-            this.buttonHapus.Size = new System.Drawing.Size(91, 36);
+            this.buttonHapus.Size = new System.Drawing.Size(81, 51);
             this.buttonHapus.TabIndex = 17;
             this.buttonHapus.Text = "Hapus";
             this.buttonHapus.UseVisualStyleBackColor = true;
@@ -71,9 +77,9 @@
             // 
             this.buttonUpdate.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.buttonUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonUpdate.Location = new System.Drawing.Point(687, 99);
+            this.buttonUpdate.Location = new System.Drawing.Point(614, 45);
             this.buttonUpdate.Name = "buttonUpdate";
-            this.buttonUpdate.Size = new System.Drawing.Size(91, 36);
+            this.buttonUpdate.Size = new System.Drawing.Size(76, 51);
             this.buttonUpdate.TabIndex = 16;
             this.buttonUpdate.Text = "Update";
             this.buttonUpdate.UseVisualStyleBackColor = true;
@@ -82,18 +88,75 @@
             // 
             this.buttonTambah.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.buttonTambah.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonTambah.Location = new System.Drawing.Point(687, 45);
+            this.buttonTambah.Location = new System.Drawing.Point(530, 45);
             this.buttonTambah.Name = "buttonTambah";
-            this.buttonTambah.Size = new System.Drawing.Size(91, 36);
+            this.buttonTambah.Size = new System.Drawing.Size(78, 51);
             this.buttonTambah.TabIndex = 15;
             this.buttonTambah.Text = "Tambah";
             this.buttonTambah.UseVisualStyleBackColor = true;
+            // 
+            // labelNamaKategori
+            // 
+            this.labelNamaKategori.AutoSize = true;
+            this.labelNamaKategori.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNamaKategori.Location = new System.Drawing.Point(13, 45);
+            this.labelNamaKategori.Name = "labelNamaKategori";
+            this.labelNamaKategori.Size = new System.Drawing.Size(114, 17);
+            this.labelNamaKategori.TabIndex = 18;
+            this.labelNamaKategori.Text = "Nama Kategori  :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 17);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Fasilitas            :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(354, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 17);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Id Fasilitas  :";
+            // 
+            // textBoxNamaKategori
+            // 
+            this.textBoxNamaKategori.Location = new System.Drawing.Point(148, 45);
+            this.textBoxNamaKategori.Name = "textBoxNamaKategori";
+            this.textBoxNamaKategori.Size = new System.Drawing.Size(376, 20);
+            this.textBoxNamaKategori.TabIndex = 21;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(148, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 20);
+            this.textBox1.TabIndex = 22;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(446, 76);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(78, 20);
+            this.textBox2.TabIndex = 23;
             // 
             // controlKategoriKamar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxNamaKategori);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelNamaKategori);
             this.Controls.Add(this.buttonHapus);
             this.Controls.Add(this.buttonUpdate);
             this.Controls.Add(this.buttonTambah);
@@ -114,5 +177,11 @@
         private System.Windows.Forms.Button buttonHapus;
         private System.Windows.Forms.Button buttonUpdate;
         private System.Windows.Forms.Button buttonTambah;
+        private System.Windows.Forms.Label labelNamaKategori;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxNamaKategori;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
