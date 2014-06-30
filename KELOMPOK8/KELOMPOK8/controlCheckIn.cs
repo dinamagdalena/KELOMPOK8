a3cff14d4db989b8ae7947a0c2c51c77f5e47a6e
dinamagdalena <mgdalenaaa@gmail.com>
2014-06-30 09:50:40 +0700

    checkIn seadanya

--- KELOMPOK8/KELOMPOK8/controlCheckIn.cs
+++ KELOMPOK8/KELOMPOK8/controlCheckIn.cs
@@ -6,14 +6,29 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace KELOMPOK8
 {
     public partial class controlCheckIn : UserControl
     {
+        //create connertion string 
+        private string strConnection = ConfigurationManager.ConnectionStrings["DbHotelConnectionString"].ConnectionString;
+
         public controlCheckIn()
         {
             InitializeComponent();
+        }
+
+        //Method for ckear form
+        private void ClearFprm()
+        {
+            textBoxNamaIn.Text = "";
+            textBoxNoKmr.Text = "";
+            textBoxNoKtp.Text = "";
+            textBoxNoTelp.Text = "";
+            textBoxNoKtp.Focus();
         }
 
         private void buttonCheckIn_Click(object sender, EventArgs e)
@@ -21,6 +36,41 @@ namespace KELOMPOK8
             string noktp = textBoxNoKtp.Text;
             string nama = textBoxNamaIn.Text;
             string notelp = textBoxNoTelp.Text;
+
+            SqlConnection connection = new SqlConnection(strConnection);
+            using (connection)
+            {
+                //Open Gate
+                connection.Open();
+
+                //create Query
+                string insert = "INSERT INTO TPelanggan (NoKtp, NamaPelanggan, NoTelpPelanggan, JenisKelaminPelanggan) values(@NoKtp, @NamaPelanggan, @NoTelpPelanggan, @JenisKelaminPelanggan) ";
+                
+                //Isnert Data Into table
+                SqlCommand cmd = new SqlCommand(insert, connection);
+
+                //Add Parameter Into CMD
+                cmd.Parameters.AddWithValue("@NoKtp", noktp);
+                cmd.Parameters.AddWithValue("@NamaPelanggan", nama);
+                cmd.Parameters.AddWithValue("@NoTelp", notelp);
+
+                // Execute Data
+                int insertResult = cmd.ExecuteNonQuery();
+
+                //Checking insert result
+                if (insertResult == 1)
+                {
+                    MessageBox.Show("CheckIn Berhasil");
+                }
+                else
+                {
+                    MessageBox.Show("Checkin gagal silahkan ulangi");
+                }
+
+                //close connection
+                connection.Close();
+
+            }
 
         }
 
