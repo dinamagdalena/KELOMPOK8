29787a1905bc4ce7add7109c09797f2a3b422f8d
iqbalap <iqbaladityap@gmail.com>
2014-07-01 07:32:00 +0700

    ngedit tabel ngehapus attribute nama kamar

--- KELOMPOK8/KELOMPOK8/controlKamar.Designer.cs
+++ KELOMPOK8/KELOMPOK8/controlKamar.Designer.cs
@@ -37,8 +37,6 @@
             this.textBoxNoKamar = new System.Windows.Forms.TextBox();
             this.textBoxJenisKamar = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBoxNamaKamar = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
             this.textBoxHargaKamar = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.textBoxJumlahKamar = new System.Windows.Forms.TextBox();
@@ -50,7 +48,7 @@
             // 
             this.buttonHapus.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.buttonHapus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonHapus.Location = new System.Drawing.Point(688, 247);
+            this.buttonHapus.Location = new System.Drawing.Point(688, 323);
             this.buttonHapus.Name = "buttonHapus";
             this.buttonHapus.Size = new System.Drawing.Size(91, 36);
             this.buttonHapus.TabIndex = 14;
@@ -61,7 +59,7 @@
             // 
             this.buttonUpdate.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.buttonUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonUpdate.Location = new System.Drawing.Point(571, 247);
+            this.buttonUpdate.Location = new System.Drawing.Point(571, 323);
             this.buttonUpdate.Name = "buttonUpdate";
             this.buttonUpdate.Size = new System.Drawing.Size(91, 36);
             this.buttonUpdate.TabIndex = 13;
@@ -72,7 +70,7 @@
             // 
             this.buttonTambah.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.buttonTambah.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonTambah.Location = new System.Drawing.Point(461, 247);
+            this.buttonTambah.Location = new System.Drawing.Point(461, 323);
             this.buttonTambah.Name = "buttonTambah";
             this.buttonTambah.Size = new System.Drawing.Size(91, 36);
             this.buttonTambah.TabIndex = 12;
@@ -102,7 +100,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(458, 45);
+            this.label2.Location = new System.Drawing.Point(458, 156);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(57, 13);
             this.label2.TabIndex = 15;
@@ -110,14 +108,14 @@
             // 
             // textBoxNoKamar
             // 
-            this.textBoxNoKamar.Location = new System.Drawing.Point(531, 38);
+            this.textBoxNoKamar.Location = new System.Drawing.Point(531, 149);
             this.textBoxNoKamar.Name = "textBoxNoKamar";
             this.textBoxNoKamar.Size = new System.Drawing.Size(69, 20);
             this.textBoxNoKamar.TabIndex = 16;
             // 
             // textBoxJenisKamar
             // 
-            this.textBoxJenisKamar.Location = new System.Drawing.Point(531, 75);
+            this.textBoxJenisKamar.Location = new System.Drawing.Point(531, 186);
             this.textBoxJenisKamar.Name = "textBoxJenisKamar";
             this.textBoxJenisKamar.Size = new System.Drawing.Size(248, 20);
             this.textBoxJenisKamar.TabIndex = 18;
@@ -125,31 +123,15 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(458, 78);
+            this.label3.Location = new System.Drawing.Point(458, 189);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(64, 13);
             this.label3.TabIndex = 17;
             this.label3.Text = "Jenis Kamar";
             // 
-            // textBoxNamaKamar
-            // 
-            this.textBoxNamaKamar.Location = new System.Drawing.Point(531, 112);
-            this.textBoxNamaKamar.Name = "textBoxNamaKamar";
-            this.textBoxNamaKamar.Size = new System.Drawing.Size(248, 20);
-            this.textBoxNamaKamar.TabIndex = 20;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(458, 115);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(65, 13);
-            this.label4.TabIndex = 19;
-            this.label4.Text = "Nama Kama";
-            // 
             // textBoxHargaKamar
             // 
-            this.textBoxHargaKamar.Location = new System.Drawing.Point(531, 151);
+            this.textBoxHargaKamar.Location = new System.Drawing.Point(531, 227);
             this.textBoxHargaKamar.Name = "textBoxHargaKamar";
             this.textBoxHargaKamar.Size = new System.Drawing.Size(131, 20);
             this.textBoxHargaKamar.TabIndex = 22;
@@ -157,7 +139,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(458, 154);
+            this.label5.Location = new System.Drawing.Point(458, 230);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(69, 13);
             this.label5.TabIndex = 21;
@@ -165,7 +147,7 @@
             // 
             // textBoxJumlahKamar
             // 
-            this.textBoxJumlahKamar.Location = new System.Drawing.Point(531, 190);
+            this.textBoxJumlahKamar.Location = new System.Drawing.Point(531, 266);
             this.textBoxJumlahKamar.Name = "textBoxJumlahKamar";
             this.textBoxJumlahKamar.Size = new System.Drawing.Size(40, 20);
             this.textBoxJumlahKamar.TabIndex = 24;
@@ -173,7 +155,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(458, 193);
+            this.label6.Location = new System.Drawing.Point(458, 269);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(73, 13);
             this.label6.TabIndex = 23;
@@ -188,8 +170,6 @@
             this.Controls.Add(this.label6);
             this.Controls.Add(this.textBoxHargaKamar);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBoxNamaKamar);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.textBoxJenisKamar);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBoxNoKamar);
@@ -218,8 +198,6 @@
         private System.Windows.Forms.TextBox textBoxNoKamar;
         private System.Windows.Forms.TextBox textBoxJenisKamar;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBoxNamaKamar;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBoxHargaKamar;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBoxJumlahKamar;
