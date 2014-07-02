e27b6e13e8f8c7dce67ceae08a8d0eb5413300ec
dinamagdalena <mgdalenaaa@gmail.com>
2014-07-03 04:43:56 +0700

    checkout

---
+++ KELOMPOK8/KELOMPOK8/Checkout.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KELOMPOK8
+{
+    class Checkout
+    {
+        public string nama;
+        public string noktp;
+        public int nokmr;
+        public string jenis_kelamin;
+    }
+}
